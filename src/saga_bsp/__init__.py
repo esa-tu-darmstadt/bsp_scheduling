fcaaa15c10470c2eadc54be976a91ae0f342e734
Tim Noack <tim@noack.id>
2025-08-26 10:42:04 +0200

    Add preprocess_task_graph to module exports

--- src/saga_bsp/__init__.py
+++ src/saga_bsp/__init__.py
@@ -4,11 +4,11 @@ from .utils.visualization import draw_bsp_gantt, draw_superstep_breakdown, draw_
 from .hardware import GraphcoreIPUHardware, create_ipu_hardware
 from .schedulers.base import BSPScheduler
 from .schedulers.async_to_bsp_scheduler import AsyncToBSPScheduler
-from .misc import SagaSchedulerWrapper
+from .misc import SagaSchedulerWrapper, preprocess_task_graph
 
 __all__ = ['AsyncSchedule', 'BSPSchedule', 'Superstep', 'BSPTask', 'BSPHardware',
            'convert_async_to_bsp', 'convert_bsp_to_async',
            'draw_bsp_gantt', 'draw_superstep_breakdown', 'draw_tile_activity',
            'GraphcoreIPUHardware', 'create_ipu_hardware',
            'BSPScheduler', 'AsyncToBSPScheduler', 'SagaSchedulerWrapper',
-           'print_bsp_schedule']
+           'print_bsp_schedule', 'preprocess_task_graph']
