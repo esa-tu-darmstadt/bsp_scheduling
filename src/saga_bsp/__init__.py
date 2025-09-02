703d7881f48be64efff6ab464c92258e598244ff
Tim Noack <tim@noack.id>
2025-09-02 09:20:17 +0200

    Add draw_busy_comm_gantt to visualization imports and __all__ exports

--- src/saga_bsp/__init__.py
+++ src/saga_bsp/__init__.py
@@ -1,6 +1,6 @@
 from .schedule import AsyncSchedule, BSPSchedule, Superstep, BSPTask, BSPHardware
 from .conversion import convert_async_to_bsp, convert_bsp_to_async
-from .utils.visualization import draw_bsp_gantt, draw_superstep_breakdown, draw_tile_activity, print_bsp_schedule
+from .utils.visualization import draw_bsp_gantt, draw_superstep_breakdown, draw_tile_activity, print_bsp_schedule, draw_busy_comm_gantt
 from .hardware import GraphcoreIPUHardware, create_ipu_hardware
 from .schedulers.base import BSPScheduler
 from .schedulers.async_to_bsp_scheduler import AsyncToBSPScheduler
@@ -8,7 +8,7 @@ from .misc import SagaSchedulerWrapper, preprocess_task_graph
 
 __all__ = ['AsyncSchedule', 'BSPSchedule', 'Superstep', 'BSPTask', 'BSPHardware',
            'convert_async_to_bsp', 'convert_bsp_to_async',
-           'draw_bsp_gantt', 'draw_superstep_breakdown', 'draw_tile_activity',
+           'draw_bsp_gantt', 'draw_superstep_breakdown', 'draw_tile_activity', 'draw_busy_comm_gantt',
            'GraphcoreIPUHardware', 'create_ipu_hardware',
            'BSPScheduler', 'AsyncToBSPScheduler', 'SagaSchedulerWrapper',
            'print_bsp_schedule', 'preprocess_task_graph']
