86b08bc34b9f9045913ebf446a3afef2ed80c9f3
tn <noack@esa.tu-darmstadt.de>
2025-09-27 14:40:23 +0200

    Refactor imports in __init__.py to include task graph utilities and remove unused hardware imports

--- src/saga_bsp/__init__.py
+++ src/saga_bsp/__init__.py
@@ -1,14 +1,18 @@
 from .schedule import AsyncSchedule, BSPSchedule, Superstep, BSPTask, BSPHardware
 from .conversion import convert_async_to_bsp, convert_bsp_to_async
 from .utils.visualization import draw_bsp_gantt, draw_superstep_breakdown, draw_tile_activity, print_bsp_schedule, draw_busy_comm_gantt
-from .hardware import GraphcoreIPUHardware, create_ipu_hardware
 from .schedulers.base import BSPScheduler
 from .schedulers.async_to_bsp_scheduler import AsyncToBSPScheduler
 from .misc import SagaSchedulerWrapper, preprocess_task_graph
+from .task_graphs import (
+    WfCommonsTaskGraphGenerator, SPNTaskGraphGenerator, TaskGraphMetadata,
+    calculate_ccr, adjust_task_graph_to_ccr, generate_ccr_variants, get_ccr_statistics
+)
 
 __all__ = ['AsyncSchedule', 'BSPSchedule', 'Superstep', 'BSPTask', 'BSPHardware',
-           'convert_async_to_bsp', 'convert_bsp_to_async'
+           'convert_async_to_bsp', 'convert_bsp_to_async',
            'draw_bsp_gantt', 'draw_superstep_breakdown', 'draw_tile_activity', 'draw_busy_comm_gantt',
-           'GraphcoreIPUHardware', 'create_ipu_hardware',
            'BSPScheduler', 'AsyncToBSPScheduler', 'SagaSchedulerWrapper',
-           'print_bsp_schedule', 'preprocess_task_graph']
+           'print_bsp_schedule', 'preprocess_task_graph',
+           'WfCommonsTaskGraphGenerator', 'SPNTaskGraphGenerator', 'TaskGraphMetadata',
+           'calculate_ccr', 'adjust_task_graph_to_ccr', 'generate_ccr_variants', 'get_ccr_statistics']
